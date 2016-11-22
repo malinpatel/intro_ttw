cc3a077bfa462e8ba5fcafd462e283620e067fdb
malin patel <malin.patel@icloud.com>
2016-11-22 14:58:24 +0000

    Added if logic to name form

--- app.rb
+++ app.rb
@@ -5,32 +5,11 @@ set    :session_secret, "super_secret"
 
   get '/named_cat' do
     p params
-    @name_randomiser =params[:name]
+    @name =params[:name]
     erb(:index)
   end
 
   get '/random_cat' do
     @name_randomiser =["Amigo", "Oscar", "Viking"].sample
     erb(:index)
-  end
-
-  get '/' do
-    "hello world"
-  end
-
-  get '/base' do
-    "all bout that base"
-  end
-
-  get '/code' do
-    "this is what my life is now.. 0011000110000001"
-  end
-
-  get '/secret' do
-    "they're behind you..."
-  end
-
-
-  get '/noob' do
-    "n00b"
   end
