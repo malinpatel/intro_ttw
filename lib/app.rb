d1ddbe65d1d5fad86436bc4f6150a1d577365583
Jack <jack_6894@hotmail.com>
2016-11-22 11:53:13 +0000

    gem shotgun add

---
+++ lib/app.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'shotgun'
+
+
+get '/' do
+  "hello world"
+end
+
+get '/base' do
+  "all bout that base"
+end
+
+get '/code' do
+  "this is what my life is now.. 0011000110000001"
+end
+
+get '/secret' do
+  "they're behind you..."
+end
+
+
+get '/noob' do
+  "n00b"
+end
