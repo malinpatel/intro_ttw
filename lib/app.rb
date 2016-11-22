d216117099983d9877fcf5050adae7a3fe1ecb80
malin patel <malin.patel@icloud.com>
2016-11-22 12:30:52 +0000

    Gems update

--- lib/app.rb
+++ lib/app.rb
@@ -4,10 +4,10 @@ get '/' do
   'Hello World'
 end
 
-# get '/secret' do
-#   "I have none..."
-# end
-#
-# get '/home' do
-#   "take me there"
-# end
+get '/secret' do
+  "I have none..."
+end
+
+get '/home' do
+  "take me there"
+end
